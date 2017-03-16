3f4cd8ed6f1c9a60a58b1286a83b03eb1e38acf5
David Atchley <atchleyd@ociweb.com>
2017-03-16 14:51:05 -0500

    fix: eslint issues in example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDom from 'react-dom';
 import ScaleText from '../../src/index';
 
@@ -9,8 +9,11 @@ const Circle = ({ text }) => (
   </div>
 </div>
 );
+Circle.propTypes = {
+  text: PropTypes.string
+};
 
-const Box = ({ text = "", centered = false, ...props }) => (
+const Box = ({ text = '', centered = false, ...props }) => (
   <div className="box">
     <ScaleText {...props}>
       <span className={`box-text ${centered ? 'centered' : ''}`}>
@@ -19,24 +22,28 @@ const Box = ({ text = "", centered = false, ...props }) => (
     </ScaleText>
   </div>
 );
+Box.propTypes = {
+  text: PropTypes.string,
+  centered: PropTypes.bool
+};
 
 const App = () => (
   <div>
     <h2>Examples</h2>
     <p>To see the component in action, resize the browser window.</p>
-    <p>The markup rendering each box below is the following:<br/>
+    <p>The markup rendering each box below is the following:<br />
     <code><pre style={{ whiteSpace: 'pre' }}>
-      <span className="line line--1">&lt;div class="box"&gt;</span><br/>
-      <span className="line line--2">  &lt;ScaleText&gt;</span><br/>
-      <span className="line line--3">    &lt;span class="box-text"&gt;</span><br/>
-      <span className="line line--4">      Box 1</span><br/>
-      <span className="line line--3">    &lt;/span&gt;</span><br/>
-      <span className="line line--2">  &lt;/ScaleText&gt;</span><br/>
-      <span className="line line--1">&lt;/div&gt;</span><br/>
+      <span className="line line--1">&lt;div class="box"&gt;</span><br />
+      <span className="line line--2">  &lt;ScaleText&gt;</span><br />
+      <span className="line line--3">    &lt;span class="box-text"&gt;</span><br />
+      <span className="line line--4">      Box 1</span><br />
+      <span className="line line--3">    &lt;/span&gt;</span><br />
+      <span className="line line--2">  &lt;/ScaleText&gt;</span><br />
+      <span className="line line--1">&lt;/div&gt;</span><br />
       </pre></code>
     </p>
     <div className="description">
-      <Circle text={1}/>
+      <Circle text={1} />
       <code style={{ float: 'left', margin: '1.5em 1em' }}>&lt;ScaleText /&gt;</code>
       <div style={{ clear: 'both' }}></div>
     </div>
@@ -45,8 +52,10 @@ const App = () => (
     </div>
 
     <div className="description">
-      <Circle text={2}/>
-      <code style={{ float: 'left', margin: '1.5em 1em' }}>&lt;ScaleText minFontSize={20} /&gt;</code>
+      <Circle text={2} />
+      <code style={{ float: 'left', margin: '1.5em 1em' }}>
+        &lt;ScaleText minFontSize={20} /&gt;
+      </code>
       <div style={{ clear: 'both' }}></div>
     </div>
     <div className="box-container">
@@ -54,8 +63,10 @@ const App = () => (
     </div>
 
     <div className="description">
-      <Circle text={3}/>
-      <code style={{ float: 'left', margin: '1.5em 1em' }}>&lt;ScaleText maxFontSize={55} /&gt;</code>
+      <Circle text={3} />
+      <code style={{ float: 'left', margin: '1.5em 1em' }}>
+        &lt;ScaleText maxFontSize={55} /&gt;
+      </code>
       <div style={{ clear: 'both' }}></div>
     </div>
     <div className="box-container">
@@ -63,8 +74,10 @@ const App = () => (
     </div>
 
     <div className="description">
-      <Circle text={4}/>
-      <code style={{ float: 'left', margin: '1.5em 1em' }}>&lt;ScaleText minFontSize={20} maxFontSize={55} /&gt;</code>
+      <Circle text={4} />
+      <code style={{ float: 'left', margin: '1.5em 1em' }}>
+        &lt;ScaleText minFontSize={20} maxFontSize={55} /&gt;
+      </code>
       <div style={{ clear: 'both' }}></div>
     </div>
     <div className="box-container">
@@ -72,7 +85,7 @@ const App = () => (
     </div>
 
     <div className="description">
-      <Circle text={5}/>
+      <Circle text={5} />
       <code style={{ float: 'left', margin: '1.5em 1em' }}>&lt;ScaleText /&gt;</code>
       <div style={{ clear: 'both' }}></div>
     </div>
