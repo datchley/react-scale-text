b803befab1df80952b863355c8f0a8ffc4b13634
Dave Atchley <david.m.atchley@gmail.com>
2017-12-19 17:37:35 -0600

    Feature/width only (#13)
    * feat(width-only): adds new `widthOnly` prop to allow fitting to width of container regardless of height overflow.
    
    * tests: updated unit test for widthOnly
    
    * tests: fixes for unit test for widthOnly

--- tests/test.spec.jsx
+++ tests/test.spec.jsx
@@ -20,6 +20,8 @@ let windowHeight = window.outerHeight;
 
 // testing utility functions
 const isOverflowing = el => el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+const isWidthOverflowing = el => el.clientWidth < el.scrollWidth;
+const isHeightOverflowing = el => el.clientHeight < el.scrollHeight;
 const getFontSize = el => parseFloat(window.getComputedStyle(el).fontSize, 10);
 const resetWindowSize = () => window.resizeTo(windowWidth, windowHeight);
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -29,7 +31,6 @@ const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 //
 
 describe('ScaleText', () => {
-
   before(() => {
     windowWidth = window.outerWidth;
     windowHeight = window.outerHeight;
@@ -41,7 +42,12 @@ describe('ScaleText', () => {
   });
 
   it('can be rendered', () => {
-    const wrapper = mount(<ScaleText className="test-1"><span>text</span></ScaleText>, options);
+    const wrapper = mount(
+      <ScaleText className="test-1">
+        <span>text</span>
+      </ScaleText>,
+      options
+    );
     expect(wrapper).to.exist;
     expect(wrapper.length).to.be.equal(1);
     wrapper.detach();
@@ -109,6 +115,50 @@ describe('ScaleText', () => {
       wrapper.update();
       const after = getFontSize(child);
       expect(parseFloat(after)).to.be.greaterThan(15);
+
+      wrapper.detach();
+      return Promise.resolve(true);
+    }
+    catch (err) {
+      return Promise.reject(err);
+    }
+  });
+
+  it("'widthOnly' fills container width with font-size of child when resized", async () => {
+    const style = {
+      display: 'inline-block',
+      width: 'calc(100px + 20vw)',
+      height: 'calc(100px + 20vw)'
+    };
+    const wrapper = mount(
+      <div className="container" style={style}>
+        <ScaleText widthOnly>
+          <span className="child">text</span>
+        </ScaleText>
+      </div>,
+      options
+    );
+    const childWrapper = wrapper.find('.child').first();
+    const child = childWrapper.getDOMNode();
+
+    try {
+      await wait(200);
+      const before = getFontSize(child);
+      window.resizeTo(windowWidth * 1.4, windowHeight * 1.4);
+
+      await wait(200);
+      wrapper.update();
+      const after = getFontSize(child);
+      expect(parseFloat(after)).to.be.gte(parseFloat(before));
+      expect(isWidthOverflowing(wrapper.getDOMNode())).to.be.false;
+      // At most the full width of the container
+      expect(parseInt(child.getBoundingClientRect().width)).to.be.at.most(
+        wrapper.getDOMNode().clientWidth
+      );
+      // At leat 3/4 of the width
+      expect(parseInt(child.getBoundingClientRect().width)).to.be.at.least(
+        wrapper.getDOMNode().clientWidth * 0.75
+      );
 
       wrapper.detach();
       return Promise.resolve(true);
